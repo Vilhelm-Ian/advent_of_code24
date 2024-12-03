3134547ae149d9cd27a58a2855aacf1e877e6c65
Vilhelm-Ian <81630959+Vilhelm-Ian@users.noreply.github.com>
2024-12-03 08:52:09 +0100

    solved day3

--- day_3/src/bin/part_2.rs
+++ day_3/src/bin/part_2.rs
@@ -1,18 +1,16 @@
 use regex::Regex;
 
 fn main() {
-    let input = include_str!("input.txt");
-    let result = solve(input);
-    println!("{result}");
+    let input = include_str!();
+    println!("{solve(input)}");
 }
 
 fn solve(input: &str) -> i32 {
     let input = format!("do(){input}don't()");
-    let dont = Regex::new(r"do\(\).*?don\'t\(\)").unwrap();
+    let dont = Regex::new(r"do\(\)(.|\n)*?don\'t\(\)").unwrap();
     let re = Regex::new(r"mul\(\d+\,\d+\)").unwrap();
     let numbers = Regex::new(r"\d+").unwrap();
     let input: String = dont.find_iter(input.as_str()).map(|ele| ele.as_str()).collect();
-    println!("{input}");
     re.find_iter(input.as_str())
         .map(|matched| {
             numbers
@@ -31,5 +29,24 @@ mod tests {
         let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         let result = solve(input);
         assert_eq!(result, 48);
+    }
+    
+        #[test]
+    fn it_works_2() {
+      let input = "do()mul(1,1)don't()do()mul(2,2)don't()";
+        let result = solve(input);
+        assert_eq!(result, 5);
+    }
+            #[test]
+    fn it_works_3() {
+      let input = "mul(1,1)don't()do()mul(1,1)do()mul(2,2)";
+        let result = solve(input);
+        assert_eq!(result, 6);
+    }
+    #[test]
+        fn it_works_4() {
+      let input = "mul(1,1)don't()mul(1,1)do()mul(1,1)do()mul(2,2)";
+        let result = solve(input);
+        assert_eq!(result, 6);
     }
 }
