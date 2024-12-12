d4a8c3b424433e798337f4cae3c0b3a99dbcb9bc
BuildTools <mariothrowsfieball@gmail.com>
2024-12-12 12:43:23 +0100

    stuck

--- day_12/src/bin/part_2.rs
+++ day_12/src/bin/part_2.rs
@@ -22,20 +22,15 @@ fn solve(input: &str) -> usize {
                 continue;
             }
             traverse([y, x], &grid, &mut seen, &mut current_area, &mut edges);
-            // name_later(&mut edges, &grid);
-            // println!("{:?}", grid[y][x]);
-            // println!("{:?}", edges.len());
-            result += current_area.len() * current_area.values().sum::<usize>();
-
-            // let ed = handle_edges(&grid, edges, grid[y][x]);
-            // let ed = how_many_a(&ed, &grid, grid[y][x]);
-            // println!("{:?} {:?}", current_area.len(), ed);
-            // result += current_area.len() * ed;
-            // println!("{:?}", ed);
+            let ed = handle_edges(&grid, edges, grid[y][x]);
+            let ed = how_many_a(&ed, &grid, grid[y][x]);
+            result += current_area.len() * ed;
+            // println!("{:?}", result);
+            // println!("{:?} len{:?}", ed, current_area.len());
             // println!("{ed} {}", grid[y][x]);
         }
     }
-    result + 1
+    result
 }
 
 type Grid = Vec<Vec<char>>;
@@ -46,9 +41,13 @@ fn parse(input: &str) -> Grid {
         .lines()
         .map(|line| {
             let mut line: Vec<char> = line.chars().collect();
+            line.insert(0, '.');
+            line.push('.');
             line
         })
         .collect();
+    grid.push(vec!['.'; grid[0].len()]);
+    grid.insert(0, vec!['.'; grid[0].len()]);
     grid
 }
 
@@ -62,106 +61,64 @@ fn is_valid_index(index: Cordinate, grid: &Grid, y: i32, x: i32) -> bool {
     true
 }
 
-// fn name_later(edges: &mut HashSet<Cordinate>, grid: &Grid) -> Vec<Cordinate> {
-//     let mut remove = vec![];
-//     let mut edges: Vec<Cordinate> = edges.clone().into_iter().collect();
-//     let mut i = 0;
-//     loop {
-//         if i >= edges.len() {
-//             break;
-//         }
-//         let key = edges[i];
-//         let mut horizontal_neighbours = 0;
-//         let mut vertical_neighbours = 0;
-//         if is_valid_index(*key, grid, 1, 0) {
-//             if grid[key[0]][key[1]] == grid[key[0] - 1][key[1]]grid[key[0]][key[1]] == grid[key[0] + 1][key[1]] {
-//                 vertical_neighbours += 1;
-//             }
-//         }
-//         if is_valid_index(*key, grid, -1, 0) {
-//             if edges.contains(grid[key[0]][key[1] - 1] {
-//                 vertical_neighbours += 1;
-//             }
-//         }
-//         if is_valid_index(*key, grid, 0, 1) {
-//             if edges.contains(grid[key[0]][key[1] + 1]) {
-//                 horizontal_neighbours += 1;
-//             }
-//         }
+fn handle_edges(grid: &Grid, edges: HashSet<Cordinate>, target: char) -> Vec<Cordinate> {
+    if edges.is_empty() {
+        return vec![];
+    }
+    let first = edges.iter().min_by_key(|[a, b]| a + b).unwrap();
+    let mut current = *first;
+    let mut seen = HashSet::new();
+    let mut list = vec![];
+    loop {
+        seen.insert(current);
+        list.push(current);
+        if let Some(next_cordinate) = next(&current, grid, &edges, &seen) {
+            current = next_cordinate;
+        } else {
+            break;
+        };
+    }
+    let mut i = 0;
+    // println!("list {:?}", list);
+    loop {
+        if i >= list.len() - 1 {
+            break;
+        }
+        while i < list.len() - 1
+            && (list[i][0] as i32 - list[i + 1][0] as i32 == 0
+                || list[i][1] as i32 - list[i + 1][1] as i32 == 0)
+        {
+            list.remove(i);
+        }
+        i += 1;
+    }
+    // println!("list {:?}", list);
+    list
+}
 
-//         if is_valid_index(*key, grid, 0, -1) {
-//             if grid[key[0]][key[1]] == grid[key[0]][key[1] - 1] {
-//                 horizontal_neighbours += 1;
-//             }
-//         }
-
-//         if horizontal_neighbours > 1 || vertical_neighbours > 1 {
-//             edges.remove(i);
-//         } else {
-//             i += 1;
-//         }
-//     }
-//     edges
-// }
-
-// fn handle_edges() -> Vec<Cordinate> {
-//     if edges.is_empty() {
-//         return vec![];
-//     }
-//     let first = edges.iter().min_by_key(|[a, b]| a + b).unwrap();
-//     let mut current = *first;
-//     let mut seen = HashSet::new();
-//     let mut list = vec![];
-//     loop {
-//         seen.insert(current);
-//         list.push(current);
-//         if let Some(next_cordinate) = next(&current, grid, &edges, &seen) {
-//             current = next_cordinate;
-//         } else {
-//             break;
-//         };
-//     }
-//     let mut i = 0;
-//     // println!("list {:?}", list);
-//     loop {
-//         if i >= list.len() - 1 {
-//             break;
-//         }
-//         while i < list.len() - 1
-//             && (list[i][0] as i32 - list[i + 1][0] as i32 == 0
-//                 || list[i][1] as i32 - list[i + 1][1] as i32 == 0)
-//         {
-//             list.remove(i);
-//         }
-//         i += 1;
-//     }
-//     // println!("list {:?}", list);
-//     list
-// }
-
-// fn how_many_a(edges: &Vec<Cordinate>, grid: &Grid, target: char) -> usize {
-//     let mut result = 0;
-//     let directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-//     for edge in edges {
-//         for direction in directions.iter() {
-//             if is_valid_index(*edge, grid, direction[0], direction[1])
-//                 && grid[(edge[0] as i32 + direction[0]) as usize]
-//                     [(edge[1] as i32 + direction[1]) as usize]
-//                     == target
-//             {
-//                 println!(
-//                     "{:?}",
-//                     [
-//                         (edge[0] as i32 + direction[0]),
-//                         (edge[1] as i32 + direction[1])
-//                     ]
-//                 );
-//                 result += 1;
-//             }
-//         }
-//     }
-//     result
-// }
+fn how_many_a(edges: &Vec<Cordinate>, grid: &Grid, target: char) -> usize {
+    let mut result = 0;
+    let directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    for edge in edges {
+        for direction in directions.iter() {
+            if is_valid_index(*edge, grid, direction[0], direction[1])
+                && grid[(edge[0] as i32 + direction[0]) as usize]
+                    [(edge[1] as i32 + direction[1]) as usize]
+                    == target
+            {
+                // println!(
+                //     "{:?}",
+                //     [
+                //         (edge[0] as i32 + direction[0]),
+                //         (edge[1] as i32 + direction[1])
+                //     ]
+                // );
+                result += 1;
+            }
+        }
+    }
+    result
+}
 
 fn next(
     cordinate: &Cordinate,
@@ -215,6 +172,7 @@ fn traverse(
     let current_value = grid[current_cordinate[0]][current_cordinate[1]];
     let directions = vec![[1, 0], [-1, 0], [0, 1], [0, -1]];
     seen.insert(current_cordinate, sides);
+    current_area.insert(current_cordinate, sides);
     for direction in directions {
         let y = direction[0];
         let x = direction[1];
