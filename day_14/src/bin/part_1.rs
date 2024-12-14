52f4363eba56a6f96d811f78249b1f0208f80460
BuildTools <mariothrowsfieball@gmail.com>
2024-12-14 08:46:17 +0100

    solved part_1

--- day_14/src/bin/part_1.rs
+++ day_14/src/bin/part_1.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use regex::Regex;
 
 struct Robot {
@@ -13,9 +15,7 @@ impl Robot {
             new_positiony = grid_height as i32 + new_positiony;
         }
         if new_positionx < 0 {
-            println!("x: {new_positionx}");
             new_positionx = grid_width as i32 + new_positionx;
-            println!("x: {new_positionx}");
         }
         if new_positiony >= grid_height as i32 {
             new_positiony = new_positiony % grid_height as i32;
@@ -25,25 +25,20 @@ impl Robot {
         }
         self.position[0] = new_positiony as usize;
         self.position[1] = new_positionx as usize;
-        println!("{:?}", self.position);
     }
 
     fn move_times(&mut self, n: i32, grid_height: usize, grid_width: usize) {
-        if self.velocity[0] >= 0 {
-            self.position[0] =
-                ((self.position[0] as i32 + self.velocity[0] * n) % grid_height as i32) as usize;
+        let y_offset = (self.position[0] as i32 + self.velocity[0] * n) % grid_height as i32;
+        let x_offset = (self.position[1] as i32 + self.velocity[1] * n) % grid_width as i32;
+        if self.velocity[0] >= 0 || y_offset == 0 {
+            self.position[0] = y_offset as usize;
         } else {
-            self.position[0] = (grid_height as i32
-                + ((self.position[0] as i32 + self.velocity[0] * n) % grid_height as i32))
-                as usize;
+            self.position[0] = (grid_height as i32 + y_offset) as usize;
         }
-        if self.velocity[1] >= 0 {
-            self.position[1] =
-                ((self.position[1] as i32 + self.velocity[1] * n) % grid_width as i32) as usize;
+        if self.velocity[1] >= 0 || x_offset == 0 {
+            self.position[1] = x_offset as usize
         } else {
-            self.position[1] = (grid_width as i32
-                + ((self.position[1] as i32 + self.velocity[1] * n) % grid_width as i32))
-                as usize;
+            self.position[1] = (grid_width as i32 + x_offset) as usize;
         }
     }
 
@@ -54,8 +49,8 @@ impl Robot {
 
 fn main() {
     let input = include_str!("./input.txt");
-    let result = solve(input);
-    // println!("{:?}", result);
+    let result = solve(input, 101, 103, 100);
+    println!("{:?}", result);
 }
 
 type Cordinate = [usize; 2];
@@ -77,8 +72,6 @@ fn parse(input: &str) -> Vec<Robot> {
                     cordinates.push(number)
                 }
             });
-            println!("{:?}", cordinates);
-            println!("{:?}", velocities);
             Robot::new(
                 [cordinates[1], cordinates[0]],
                 [velocities[1], velocities[0]],
@@ -88,25 +81,66 @@ fn parse(input: &str) -> Vec<Robot> {
     robots
 }
 
-fn solve(input: &str) -> Vec<Robot> {
+fn solve(input: &str, width: usize, height: usize, iterations: i32) -> i32 {
     let mut robots = parse(input);
-    for i in 0..5 {
-        for robot in robots.iter_mut() {
-            robot.move_robot(7, 11)
+    for robot in robots.iter_mut() {
+        robot.move_times(iterations, height, width)
+    }
+    let mut grid = vec![vec![0; width]; height];
+    for robot in robots.iter() {
+        let [y, x] = robot.position;
+        grid[y][x] += 1;
+    }
+    fill_quadrants(height, width, &robots)
+}
+
+fn fill_quadrants(grid_height: usize, grid_width: usize, robots: &Vec<Robot>) -> i32 {
+    let mut first_quardant = 0;
+    let mut second_quardant = 0;
+    let mut third_quardant = 0;
+    let mut fourth_quardant = 0;
+    for robot in robots {
+        if robot.position[0] < grid_height / 2 {
+            if robot.position[1] < grid_width / 2 {
+                first_quardant += 1;
+            }
+            if robot.position[1] > grid_width / 2 {
+                second_quardant += 1;
+            }
+        }
+        if robot.position[0] > grid_height / 2 {
+            if robot.position[1] < grid_width / 2 {
+                third_quardant += 1;
+            }
+            if robot.position[1] > grid_width / 2 {
+                fourth_quardant += 1;
+            }
         }
     }
-    robots
+    // println!("{first_quardant} {second_quardant} {third_quardant} {fourth_quardant}");
+    first_quardant * second_quardant * third_quardant * fourth_quardant
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    // #[test]
-    // fn it_works() {
-    //     let input = "p=2,4 v=2,-3";
-    //     let result = solve(input);
-    //     assert_eq!(result[0].position, [3, 1]);
-    // }
+    pub const INPUT: &str = "p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3";
+    #[test]
+    fn it_works() {
+        let result = solve(INPUT, 11, 7, 100);
+        assert_eq!(result, 12);
+    }
     // #[test]
     // fn it_works() {
     //     let input = "p=2,4 v=2,-3";
