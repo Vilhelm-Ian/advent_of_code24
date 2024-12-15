86682d0b51cdb697b85d09c4fbf90be2d33b613e
BuildTools <mariothrowsfieball@gmail.com>
2024-12-15 14:51:12 +0100

    solved part_2

--- day_15/src/bin/part_2.rs
+++ day_15/src/bin/part_2.rs
@@ -1,6 +1,10 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("./input.txt");
     let result = solve(input);
+    //1539130 too high
+    //1523849 too low
     println!("{:?}", result);
 }
 
@@ -88,8 +92,8 @@ fn move_element(
     grid: &mut Grid,
     instruction: char,
     current_cordinate: Cordinate,
-    mut second: bool,
 ) -> Option<Cordinate> {
+    let og_grid = grid.clone();
     let current_square = index_grid(current_cordinate, grid);
     let direction = match_movement(instruction);
     let next_cordinate = update_cordinate(current_cordinate, direction);
@@ -102,40 +106,42 @@ fn move_element(
         current_cordinate_2 = match current_square {
             ']' => Some(update_cordinate(current_cordinate, [0, -1])),
             '[' => Some(update_cordinate(current_cordinate, [0, 1])),
-            _ => Some([0, 0]),
+            _ => None,
         };
-        current_square_2 = Some(index_grid(current_cordinate_2.unwrap(), grid));
-        if current_cordinate_2.unwrap() != [0, 0] {
+        if current_cordinate_2.is_some() {
+            current_square_2 = Some(index_grid(current_cordinate_2.unwrap(), grid));
             next_cordinate_2 = Some(update_cordinate(current_cordinate_2.unwrap(), direction));
             next_square_2 = Some(index_grid(next_cordinate_2.unwrap(), grid));
-            // println!(
-            //     "current2 {:?} current1 {:?}",
-            //     current_cordinate_2, current_cordinate
-            // );
+            if next_square_2.unwrap() == '#' {
+                return None;
+            }
         }
-        second = true;
     }
     if next_square == '#'
-        || (next_square != '.' && move_element(grid, instruction, next_cordinate, false).is_none())
+        || (next_square != '.' && move_element(grid, instruction, next_cordinate).is_none())
     {
+        for y in 0..grid.len() {
+            for x in 0..grid[0].len() {
+                grid[y][x] = og_grid[y][x];
+            }
+        }
         return None;
     }
     if let Some(next_cordinate_2) = next_cordinate_2 {
-        // println!("here");
-        // println!("next2 {:?} next1 {:?}", next_cordinate_2, next_cordinate);
         if matches!(
             (next_square_2.unwrap(), current_square_2.unwrap()),
             ('[', ']') | (']', '[')
-        ) && move_element(grid, instruction, next_cordinate_2, false).is_none()
+        ) && move_element(grid, instruction, next_cordinate_2).is_none()
         {
-            return None;
-        }
-        if next_square_2.unwrap() == '#' {
+            for y in 0..grid.len() {
+                for x in 0..grid[0].len() {
+                    grid[y][x] = og_grid[y][x];
+                }
+            }
             return None;
         }
         grid[next_cordinate_2[0]][next_cordinate_2[1]] = current_square_2.unwrap();
         grid[current_cordinate_2.unwrap()[0]][current_cordinate_2.unwrap()[1]] = '.';
-        // println!("here2");
     };
     grid[next_cordinate[0]][next_cordinate[1]] = current_square;
     grid[current_cordinate[0]][current_cordinate[1]] = '.';
@@ -158,26 +164,28 @@ fn solve(input: &str) -> i32 {
     let (mut grid, instructions) = parse(input);
     let mut grid = duplicate_map(&grid);
     let mut current_cordinate = find_robot(&grid);
+    let mut set = HashSet::new();
     for instruction in instructions {
-        let new_cordiante = move_element(&mut grid, instruction, current_cordinate, false);
-        println!("{instruction}");
-        for (i, mut line) in grid.iter().enumerate() {
-            let mut l = line.clone();
-            if line.iter().any(|l| *l == '@') {
-                l.extend_from_slice(&['.', '.', instruction]);
-            }
-            l.insert(0, char::from_digit(i as u32 % 10, 10).unwrap());
-            let l: String = l.iter().collect();
-            println!("{:?}", l);
-        }
+        let new_cordiante = move_element(&mut grid, instruction, current_cordinate);
 
         if let Some(new_cordiante) = new_cordiante {
-            let m: String = (0..grid[0].len())
-                .map(|digit| char::from_digit(digit as u32 % 10, 10).unwrap())
-                .collect();
-            println!("{:?}", m);
-            println!("\n");
             current_cordinate = new_cordiante;
+        } else {
+            // println!("{instruction}");
+            let grid_clone = grid.clone();
+            if set.contains(&grid_clone) {
+                continue;
+            }
+            set.insert(grid_clone);
+            // for (i, mut line) in grid.iter().enumerate() {
+            //     let mut l = line.clone();
+            //     if line.iter().any(|l| *l == '@') {
+            //         l.extend_from_slice(&['.', '.', instruction]);
+            //     }
+            //     l.insert(0, char::from_digit(i as u32 % 10, 10).unwrap());
+            //     let l: String = l.iter().collect();
+            //     println!("{:?}", l);
+            // }
         }
     }
     calculate_score(&grid)
@@ -225,6 +233,7 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 ##..........##
 ##..........##
 ##############";
+
     #[test]
     fn test_2_1() {
         let result = solve(INPUT_BIG);
@@ -237,8 +246,7 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
         let mut grid = duplicate_map(&grid);
         let mut current_cordinate = find_robot(&grid);
         for instruction in instructions {
-            if let Some(cordinate) = move_element(&mut grid, instruction, current_cordinate, false)
-            {
+            if let Some(cordinate) = move_element(&mut grid, instruction, current_cordinate) {
                 current_cordinate = cordinate;
             }
         }
